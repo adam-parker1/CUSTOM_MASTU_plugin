4303928d6973b2dfa2af107904ad8eb89db868e8
sdixon <stephen.dixon@ukaea.uk>
2025-09-23 00:14:01 +0100

    initial implementation of client result caching

--- custom_mastu.cpp
+++ custom_mastu.cpp
@@ -1,22 +1,29 @@
 #include "custom_mastu.h"
 
+#include <deque>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+#include <sstream>
+#include <stdexcept>
+#include <unordered_map>
+#include <optional>
+#include <functional>
+
 #include <boost/algorithm/string.hpp>
+#include <fmt/format.h>
+
+// UDA includes
 #include <c++/UDA.hpp>
 #include <clientserver/initStructs.h>
 #include <clientserver/stringUtils.h>
 #include <clientserver/udaStructs.h>
 #include <clientserver/udaTypes.h>
-#include <fmt/format.h>
 #include <plugins/pluginStructs.h>
 #include <plugins/udaPlugin.h>
 
 #include "ext_include/nlohmann/json.hpp"
 #include "utils/uda_plugin_helpers.hpp"
-#include <deque>
-#include <fstream>
-
-#include <cmath>
-#include <sstream>
 
 class CustomMastuPlugin {
   public:
@@ -25,6 +32,8 @@ class CustomMastuPlugin {
         if (!init_ || STR_IEQUALS(request->function, "init") || STR_IEQUALS(request->function, "initialise")) {
             reset(plugin_interface);
             // Initialise plugin
+            const char* cache = std::getenv("UDA_GEOM_PLUGIN_CLIENT_CACHE");
+            cache_enabled_ = (cache == nullptr) or (std::stoi(cache) > 0);
             init_ = true;
         }
     }
@@ -48,7 +57,14 @@ class CustomMastuPlugin {
     int pf_conn_matrix(IDAM_PLUGIN_INTERFACE* interface);
 
   private:
+    [[nodiscard]] std::string make_cache_key(std::string_view signal, std::string_view source, std::string_view host, int port) const;
+    [[nodiscard]] std::optional<std::reference_wrapper<const uda::Result>> check_cache(const std::string& key) const;
+     std::optional<std::reference_wrapper<const uda::Result>>get_data(std::string_view signal, std::string_view source, std::string_view host, int port);
+
     bool init_ = false;
+    std::unordered_map<std::string, const uda::Result&> cache_;
+    bool cache_enabled_ = true;
+
 };
 
 // placeholder pf_passive_geometry
@@ -166,13 +182,15 @@ float get_float_var(uda::TreeNode& final_tree, std::string_view var_str, int ele
 
     auto result = std::find(anames.begin(), anames.end(), var_str);
     if (result == anames.end()) {
-        return false;
+        // return false;
+        throw std::runtime_error("ADAM PUT AN ERROR MESSAGE HERE");
     }
 
     long idx{std::distance(anames.begin(), result)};
 
     if (arank[idx] != 1) {
-        return 999.;
+        // return 999.;
+        throw std::runtime_error("ADAM PUT AN ERROR MESSAGE HERE");
     }
 
     // always a float for this case, don't panic
@@ -287,6 +305,51 @@ int custom_passive(IDAM_PLUGIN_INTERFACE* interface, uda::TreeNode& final_tree,
     return return_code;
 }
 
+std::string CustomMastuPlugin::make_cache_key(std::string_view signal, std::string_view source, std::string_view host, int port) const
+{
+    if (!cache_enabled_) {
+        return {};
+    }
+    // start with signal as most likely to change most frequently
+    return fmt::format("{}|{}|{}|{}", signal, source, host, port);
+}
+
+std::optional<std::reference_wrapper<const uda::Result>> CustomMastuPlugin::check_cache(const std::string& key) const
+{
+    if (!cache_enabled_) {
+        return std::nullopt;
+    }
+
+    auto result = cache_.find(key);
+    if (result != cache_.end()) {
+        return std::cref(result->second);
+    }
+    return std::nullopt;
+}
+
+std::optional<std::reference_wrapper<const uda::Result>> CustomMastuPlugin::get_data(std::string_view signal, std::string_view source, std::string_view host, int port) 
+{
+    static uda::Client client;
+
+    auto cache_key = make_cache_key(signal, source, host, port);
+    auto maybe_result = check_cache(cache_key);
+    if (maybe_result.has_value()){
+        return maybe_result;
+    }
+
+    uda::Client::setServerHostName(host.data());
+    uda::Client::setServerPort(port);
+
+    const uda::Result& data = client.get(signal.data(), source.data());
+    // Check for errors
+    if (data.errorCode() != uda::OK) {
+        // RAISE_PLUGIN_ERROR("uda::Result data is not uda::OK");
+        throw std::runtime_error("uda::Result data is not uda::OK");
+    }
+
+    return std::cref(data);
+}
+
 int CustomMastuPlugin::custom_passive_structures(IDAM_PLUGIN_INTERFACE* interface) {
 
     //////////////////////////////////////////////////////////////
@@ -317,35 +380,24 @@ int CustomMastuPlugin::custom_passive_structures(IDAM_PLUGIN_INTERFACE* interfac
     int element{999};
     FIND_INT_VALUE(request_data->nameValueList, element);
 
-    static uda::Client client;
-    uda::Client::setServerHostName(host_str);
-    uda::Client::setServerPort(port);
-
-    // eg. GEOM::get(signal=/magnetics/pfcoil/d1_upper, Config=1);
     std::transform(signal_str.begin(), signal_str.end(), signal_str.begin(), ::tolower);
-
     std::string geom_request = fmt::format("GEOM::get(signal={}, Config=1)", signal_str);
-    // std::string geom_request = fmt::format("GEOM::get(signal={}, cal=1)", signal_str);
 
-    std::deque<std::string> split_vec{split_request(key_str)};
-    const uda::Result& data = client.get(geom_request, std::to_string(source));
+    auto maybe_result = get_data(signal, std::to_string(source), host, port); //throws
 
-    // Check for errors
-    if (data.errorCode() != uda::OK) {
-        RAISE_PLUGIN_ERROR("uda::Result data is not uda::OK");
-    }
-
-    // Check this returned data is a structure: Set (register) the data tree for accessors
+    const uda::Result& data = maybe_result->get();
     if (!data.isTree()) {
         RAISE_PLUGIN_ERROR("Returned data is not of expected tree structure");
     }
 
+    std::deque<std::string> split_vec{split_request(key_str)};
+
     uda::TreeNode root_tree = data.tree();
     // Hack to skip two levels
-    if (!tree_check(root_tree)) {
+    if (!static_cast<bool>(tree_check(root_tree))) {
         root_tree = root_tree.child(0);
     }
-    if (!tree_check(root_tree)) {
+    if (!static_cast<bool>(tree_check(root_tree))) {
         root_tree = root_tree.child(0);
     }
 
@@ -367,6 +419,10 @@ int CustomMastuPlugin::pf_coil_current(IDAM_PLUGIN_INTERFACE* interface) {
     data_block->rank = 0;
     data_block->dims = nullptr;
 
+    int port{0};
+    FIND_REQUIRED_INT_VALUE(request_data->nameValueList, port);
+    const char* host{nullptr};
+    FIND_REQUIRED_STRING_VALUE(request_data->nameValueList, host);
     int source{0};
     FIND_REQUIRED_INT_VALUE(request_data->nameValueList, source);
     const char* signal{nullptr};
@@ -375,24 +431,23 @@ int CustomMastuPlugin::pf_coil_current(IDAM_PLUGIN_INTERFACE* interface) {
 
     std::deque<std::string> split_signal;
     boost::split(split_signal, signal_str, boost::is_any_of("/"));
-    int error_code{1};
-
-    std::stringstream request;
-    request << "UDA::get(signal=" << signal_str << ",source=" << source << ")";
-    const auto request_str = request.str();
-
     if (split_signal.back() == "PC") {
         std::vector<float> temporary_vector{0.};
-        error_code = imas_json_plugin::uda_helpers::setReturnDataArrayType_Vec(data_block, temporary_vector);
-    } else {
-        error_code = callPlugin(interface->pluginList, request_str.c_str(), interface);
-        if (split_signal.back() == "P1") {
-            auto* data = reinterpret_cast<float*>(data_block->data);
-            const size_t array_size(data_block->data_n);
-            const auto span = gsl::span{data, array_size};
-            std::for_each(span.begin(), span.end(), [&](float& elem) { elem *= 0.5; });
-            error_code = 0;
-        }
+        return imas_json_plugin::uda_helpers::setReturnDataArrayType_Vec(data_block, temporary_vector);
+    }
+
+    int error_code{1};
+
+    auto maybe_result = get_data(signal, std::to_string(source), host, port); //throws
+    const uda::Result& data = maybe_result->get();
+
+    // error_code = callPlugin(interface->pluginList, request_str.c_str(), interface);
+    if (split_signal.back() == "P1") {
+        auto* data = reinterpret_cast<float*>(data_block->data);
+        const size_t array_size(data_block->data_n);
+        const auto span = gsl::span{data, array_size};
+        std::for_each(span.begin(), span.end(), [&](float& elem) { elem *= 0.5; });
+        error_code = 0;
     }
 
     return error_code;
